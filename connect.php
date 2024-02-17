a776ff49d1091048275eda758d0fd4737cd909ff
safiul azam <safiul.az@gmail.com>
2024-02-17 10:56:36 +0600

    third commit

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$con=mysqli_connect('localhost','root','','signupforms_2');
+if(!$con){
+    die(mysqli_query($con));
+}
+
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,93 @@
+<?php
+$user=0;
+$invalid=0;
+if($_SERVER['REQUEST_METHOD']=="POST"){
+  include 'connect.php';
+  $username=$_POST['username'];
+  $password=$_POST['password'];
+
+  $sql="select * from `registration` where `username`='$username' and `password`='$password'";
+  $execute=mysqli_query($con,$sql);
+  if($execute){
+    $num=mysqli_num_rows($execute);
+    if($num>0){
+      // echo "Datbase Already Exist";
+      $user=1;
+      session_start();
+      $_SESSION['username']=$username;
+      header('location:home.php');
+    }else{
+      $invalid=1;
+    }
+  }
+}
+
+?>
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Login Forms</title>
+  </head>
+  <body>
+  <?php
+  if($user){
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Success </strong>Welcome
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>';
+  }
+  
+  
+  ?>
+
+
+<?php
+if($invalid){
+  echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Sorry</strong> Not Found
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+}
+
+
+
+?>
+
+
+
+
+
+    <h1 class="text-center mt-5">Login Forms</h1>
+    <div class="container mt-5">
+    <form action="login.php" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name Here</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" name="username" placeholder="Name">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary w-100">Login</button>
+</form>
+    </div>
+
+    
+  </body>
+</html>
